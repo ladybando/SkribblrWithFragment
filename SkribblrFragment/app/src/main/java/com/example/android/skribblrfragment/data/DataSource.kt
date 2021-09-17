dc81f1e6324e02a88cc1b566d1385e219555f0b5
Ayana Bando <ayanaflax@yahoo.com>
2021-09-17 19:35:35 -0400

    update to add fragments to notes app

---
+++ SkribblrFragment/app/src/main/java/com/example/android/skribblrfragment/data/DataSource.kt
@@ -0,0 +1,9 @@
+package com.example.android.skribblrfragment.data
+
+class DataSource  {
+    //should function also accept a string as an argument? note?
+    fun skribblList(): MutableList<String>{
+        return mutableListOf()
+    }
+
+}
