7a2c3ae230bd46162a00036ee364b0385f6e289a
Ayana Bando <ayanabando@gmail.com>
2021-09-27 19:30:30 -0400

    Update TaskViewAdapter.kt
    add interface for onclick listener use in main activity

--- app/src/main/java/com/example/android/skribblrfragment/adapter/TaskViewAdapter.kt
+++ app/src/main/java/com/example/android/skribblrfragment/adapter/TaskViewAdapter.kt
@@ -1,56 +1,63 @@
 package com.example.android.skribblrfragment.adapter
 
 import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.skribblrfragment.INTENT_DATA_NAME
-import com.example.android.skribblrfragment.ListOfItemsActivity
 import com.example.android.skribblrfragment.R
-import com.example.android.skribblrfragment.databinding.ActivityListOfItemsBinding
-import com.example.android.skribblrfragment.databinding.ListItemBinding
+import com.example.android.skribblrfragment.databinding.ItemLayoutBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class TaskViewAdapter(
+    private val listener: Listener,
     private val context: Context,
-    private val dataSet: MutableList<String>,
+    private var dataSet: MutableList<String>
 ) :
     RecyclerView.Adapter<TaskViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        with(holder) {
-            binding.listOfItemsView.text = dataSet[position]
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = with(holder) {
+        binding.listOfItemsView.text = dataSet[position]
 
-            binding.listOfItemsView.setOnClickListener {
-                val intent = Intent(context, ListOfItemsActivity::class.java)
-                intent.putExtra(INTENT_DATA_NAME, dataSet[position])
-                context.startActivity(intent)
-            }
-            binding.listOfItemsView.setOnLongClickListener {
-                MaterialAlertDialogBuilder(context)
-                    .setTitle(R.string.title)
-                    .setMessage(R.string.affirmation)
-                    .setCancelable(false)
-                    .setNegativeButton(R.string.decline) { dialog, _ ->
-                        dialog.dismiss()
-                    }
-                    .setPositiveButton(R.string.accept) { _, _ ->
-                        dataSet.removeAt(position)
-                        notifyItemRemoved(position)
-                    }
-                    .show()
-                true
-            }
+        binding.listOfItemsView.setOnLongClickListener {
+            MaterialAlertDialogBuilder(context)
+                .setTitle(R.string.title)
+                .setMessage(R.string.affirmation)
+                .setCancelable(false)
+                .setNegativeButton(R.string.decline) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .setPositiveButton(R.string.accept) { _, _ ->
+                    dataSet.removeAt(position)
+                    notifyItemRemoved(position)
+                }
+                .show()
+            true
         }
     }
 
     override fun getItemCount(): Int = dataSet.size
 
-    class ViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class ViewHolder(val binding: ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root){
+        private val textView = binding.listOfItemsView
+
+        init {
+            textView.setOnClickListener {
+                listener.onTaskClicked(adapterPosition)
+            }
+        }
+    }
+
+    fun setTasks(listOfTask:MutableList<String>) {
+        this.dataSet = listOfTask
+        notifyItemChanged(0)
+    }
+
+    interface Listener{
+        fun onTaskClicked(index: Int)
+    }
 }
