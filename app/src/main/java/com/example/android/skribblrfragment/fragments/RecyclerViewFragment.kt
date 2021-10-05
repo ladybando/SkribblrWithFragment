3f83e1c51dfda4ae21244e862926208f54670d9d
Ayana Bando <ayanabando@gmail.com>
2021-10-05 07:44:18 -0400

    Update RecyclerViewFragment.kt

--- app/src/main/java/com/example/android/skribblrfragment/fragments/RecyclerViewFragment.kt
+++ app/src/main/java/com/example/android/skribblrfragment/fragments/RecyclerViewFragment.kt
@@ -61,7 +61,7 @@ class RecyclerViewFragment : Fragment(), TaskViewAdapter.Listener {
         super.onResume()
         val editedInput = args.editUserInput
         val position = viewModel.listPosition
-        // Don't attempt the update any items in the list unless we have previously set a position
+        // Don't attempt to update any items in the list unless we have previously set a position
         // for the item that needs to be updated
         if (position != -1) {
             viewModel.taskList[position] = editedInput!!
